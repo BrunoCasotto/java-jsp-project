6dd3ea7c0700d319d229d7ae629c15f8c03bcf35
brunocasotto <casottoalves@hotmail.com>
2018-03-16 21:07:46 -0300

    add login xhtml

---
+++ WebContent/WEB-INF/classes/beans/Login.java
@@ -0,0 +1,7 @@
+package br.todo.beans;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Login {
+
+}
---
+++ src/br/ifsp/todo/bean/Login.java
@@ -0,0 +1,12 @@
+package br.ifsp.todo.bean;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Login {
+	private int numero;
+
+	public int getNumero() {
+		return numero;
+	}
+
+}
